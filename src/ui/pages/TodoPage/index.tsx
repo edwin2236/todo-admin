ad4c57ff79d31c7aa7e0fb5641cd65e1ef4f7b8d
VG <ing.vinciwar@gmail.com>
2023-02-10 15:41:07 -0500

    integrate data grid in ToDo

--- src/ui/pages/TodoPage/index.tsx
+++ src/ui/pages/TodoPage/index.tsx
@@ -4,7 +4,11 @@ import { TaskService } from '../../../data/services/task.service'
 import { GetAllTaskUseCase } from '../../../domain'
 import Header from '../../components/Header'
 
+import { Box } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+
 function TodoPage() {
+  
   const [data, setData] = useState<Task[]>([])
   const useCase = new GetAllTaskUseCase(new TaskService())
 
@@ -14,12 +18,32 @@ function TodoPage() {
     })
   }, [])
 
+  const columns = [
+    {
+      field: 'title',
+      headerName: 'Todo',
+      width: 550,
+      editable: true,
+    }
+  ];
+
   return (
     <>
       <Header />
-      {data.map((item) => (
-        <div key={item.id}>{item.title}</div>
-      ))}
+
+      <Box sx={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', marginTop: '20px'}}>
+        <Box sx={{ height: 371, width: '50%', }}>
+            <DataGrid
+              rows={data}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              checkboxSelection
+              disableSelectionOnClick
+              experimentalFeatures={{ newEditingApi: true }}
+            />
+          </Box>
+      </Box>
     </>
   )
 }
