e1b7cfb17b8e045ea41e75bca216962ee81d4e1c
Edwin Castaño <edwin_2236@hotmail.com>
2023-02-15 18:06:47 -0500

    enhancement: add barrel and refactor for header component

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -1,37 +1,29 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import * as React from 'react'
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Button,
+  MenuItem,
+} from '@mui/material'
+import { Adb as AdbIcon, Menu as MenuIcon } from '@mui/icons-material'
 
-const pages = ['About', 'Blog', 'Users'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ['About', 'Blog', 'Users']
 
 function Header() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    setAnchorElNav(event.currentTarget)
+  }
 
   const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    setAnchorElNav(null)
+  }
 
   return (
     <AppBar position="static">
@@ -126,6 +118,6 @@ function Header() {
         </Toolbar>
       </Container>
     </AppBar>
-  );
+  )
 }
-export default Header;
+export default Header
--- src/ui/pages/TodoPage/index.tsx
+++ src/ui/pages/TodoPage/index.tsx
@@ -1,6 +1,6 @@
 import { Header } from '../../components'
 import { useTask } from '../../hooks/useTask'
-// enhacement
+
 function TodoPage() {
   const { tasks } = useTask()
 
